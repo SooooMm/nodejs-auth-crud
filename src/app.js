ebdce3f1b38537c27feffee1e8ce947f00d628ea
soooomm00@gmail.com <soooomm00@gmail.com>
2024-05-28 21:27:58 +0900

    Feat : 이력서 관리 기능 개발
    API 추가 목록
    - 이력서 생성
    - 이력서 목록 조회
    - 이력서 상세 조회
    - 이력서 수정
    - 이력서 삭제
    
    Resolves: #4

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import "./utils/prisma.util.js";
 import ErrorHandlingMiddleware from "./middlewares/error-handler.middleware.js";
 import AuthRouter from "./routers/auth.router.js";
 import UserRouter from "./routers/users.router.js";
+import ResumeRouter from "./routers/resumes.router.js";
 
 dotenv.config();
 
@@ -13,7 +14,7 @@ const PORT = 3018;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api", [AuthRouter, UserRouter]);
+app.use("/api", [AuthRouter, UserRouter, ResumeRouter]);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
